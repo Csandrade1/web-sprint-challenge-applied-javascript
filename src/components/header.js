966e3db048a9c9d62543714bf1d025e2c4152492
Csandrade1 <andrade.cynthia07@gmail.com>
2022-04-15 14:55:40 -0700

    still need card.js

--- src/components/header.js
+++ src/components/header.js
@@ -11,15 +11,36 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
-}
+  const divHeader = document.createElement("div");
+  const dateContent = document.createElement("span");
+  const titleContent = document.createElement("h1");
+  const tempContent = document.createElement("span");
+
+  divHeader.appendChild(dateContent);
+  divHeader.appendChild(titleContent);
+  divHeader.appendChild(tempContent);
+
+  divHeader.classList.add("header");
+  dateContent.classList.add("date");
+  tempContent.classList.add("temp");
+
+  dateContent.textContent = date;
+  titleContent.textContent = title;
+  tempContent.textContent = temp;
+
+  return divHeader;
+};
 
 const headerAppender = (selector) => {
+  const selectorArg = document.querySelector(selector);
+  const newHeader = Header("Andrade Times", "March 10, 2022", "43°");
+  selectorArg.append(newHeader);
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-}
+};
 
-export { Header, headerAppender }
+export { Header, headerAppender };
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,21 +1,41 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
-  // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
-}
+  const divTopics = document.createElement("div");
+  divTopics.classList.add("topics");
+  for (let i = 0; i < topics.length; i++) {
+    const topicType = document.createElement("div");
+    topicType.classList.add("tab");
+    topicType.textContent = topics[i];
+    divTopics.appendChild(topicType);
+  }
+  return divTopics;
+};
+// TASK 3
+// ---------------------
+// Implement this function which takes an array of strings ("topics") as its only argument.
+// As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
+// then the function returns the markup below.
+// The tags used, the hierarchy of elements and their attributes must match the provided markup!
+// The text inside elements will be set using their `textContent` property (NOT `innerText`).
+//
+// <div class="topics">
+//   <div class="tab">javascript</div>
+//   <div class="tab">bootstrap</div>
+//   <div class="tab">technology</div>
+// </div>
+//
 
 const tabsAppender = (selector) => {
+  const data = axios
+    .get("http://localhost:5001/api/topics")
+    .then((resp) => {
+      console.log(resp.data.topics);
+      document.querySelector(selector).appendChild(Tabs(resp.data.topics));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   // TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
@@ -23,6 +43,6 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
-}
+};
 
-export { Tabs, tabsAppender }
+export { Tabs, tabsAppender };
